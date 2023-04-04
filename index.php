5ab2c4049e8eba1e726e15437dd22cbf49b4a463
AMike01 <47867706+AMike01@users.noreply.github.com>
2023-04-04 09:08:37 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="utf-8" />
+        <title>Vetrina articoli di Vetro</title>
+        <meta name="description" content="Vetrina di articoli di vetro come vasi, lampadari, specchi, bicchieri, e piccoli oggetti " /> <!-- Aiuta a farsi trovare sui motori di ricerca-->
+        <meta name="keywords" content="vetro, vasi, lampadari, specchi, bicchieri" /> <!-- Aiuta a farsi trovare sui motori di ricerca-->
+        <meta name="author" content="Web Triplet (?) (Alberto Michelazzo)" />
+
+       <!-- <link rel="stylesheet" href="style.css" media="handheld, screen" /> -->
+
+        
+        <link rel="shortcut icon" type="image/png" href="./images/chandelier.ico" />
+    </head>
+    <body>
+        <header>
+            <h1>Vetrina Articoli di Vetro</h1>
+        </header>
+        <nav id="breadcrumb">
+            <p>Ti trovi in: <span lang="en">Home</span></p>
+        </nav>
+
+        <nav id="menu">
+            <ul>
+                <li id="currentLink"><span lang="en">Homepage</span></li>
+                <li><a href="prodotti.html">Prodotti</a></li>
+                <li><a href="chi_siamo.html">Chi siamo</a></li>
+            </ul>
+        </nav>
+
+        <main>
+            
+
+            <p>Negozio Fisico per un Maestro Vetraio di Murano.
+                Ogni prodotto visibile nel sito è un prodotto in vetrina di un “azienda/artista” e sarà possibile chiedere informazioni a riguardo.
+                La funzione della vetrina è di sola esposizione al pubblico e non di vendita diretta, nel caso si è interessati basti contattare per informazioni.
+
+            </p>
+            
+            <p>Il negozio offre anche un servizio di personalizzazione.
+            </p>
+
+
+        </main>
+        <footer id="footer">
+           <p>Web Triplet & Alberto Michelazzo - <span lang="en">All Rights Reserved</span></p>
+        </footer>   
+    </body>
+</html>
