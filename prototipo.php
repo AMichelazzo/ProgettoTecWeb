f3763905a4613b9a0fc06489d83421f2f23e5211
Alessio <101570067+alessiobarraco@users.noreply.github.com>
2023-04-17 18:26:44 +0200

    modifica link prototipo

--- prototipo.php
+++ prototipo.php
@@ -2,6 +2,6 @@
 $pagina = file_get_contents("HTML/prototipo.html");
 $target = "Nome negozio";
 $titolo = "TITOLO";
-$pagina = str_replace($target,$titolo,$pagina);
+$pagina = str_replace($target, $titolo, $pagina);
 echo($pagina);
 ?>
