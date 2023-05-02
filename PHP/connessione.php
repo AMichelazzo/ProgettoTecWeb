6492e9168a04561f5012ee79b08f46d03e02eaf0
AMike01 <47867706+AMike01@users.noreply.github.com>
2023-05-02 12:49:18 +0200

    Aggiunta pagina html e php "Messaggio"

--- PHP/connessione.php
+++ PHP/connessione.php
@@ -54,6 +54,19 @@ class DBAccess {
     return $result;
   }
   
+
+  public function getMessage($id_messaggio) {
+    $OKMess=pulisciInput($id_messaggio);
+    $query = "SELECT id_messaggio, username, msg  FROM 'messaggi'";  // da implementare con il resto dei parametri di messaggi
+    $queryResult = mysqli_query($this->connection, $query);
+    %result = array();
+    while ($row = mysqli_fetch_assoc($queryResult)) {
+        $result[] = $row;
+    }
+    return $result;
+  }
+
+
   public function closeConnection() {
     mysqli_close($this->connection);
   }
---
+++ PHP/messaggio.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "connessione.php";
+use DB\DBAccess;
+
+$connessione = new DBAccess();
+
+if (isset($_GET["mess"])) { 
+    $result = $id = $username = $msg = $replace ="";
+    $connessioneRiuscita = $connessione->openDBConnection();
+    $result=$connessione->getMessage($_GET["mess"]);
+    $connessione->closeConnection();
+    if(count($result)>0){
+        $id=$result[0]['Id'];
+        $username=$result[0]['Username'];
+        $msg=$result[0]['Msg'];
+      
+    
+        $replace = array("Id Messaggio" =>$id,
+                            "Username Utente" =>$username,
+                            "Messaggio Utente" =>$msg);
+                      
+    }
+    else{
+        header("Location: ../PHP/categorie.php");
+    }
+
+    $paginaHTML = file_get_contents("../HTML/messaggio.html");
+        foreach($replace as $key => $value)
+                $paginaHTML = str_replace($key, $value, $paginaHTML);
+        echo $paginaHTML;
+}
+?>
