dcf336932928390d0ec216a878e31799287bcd4f
AMike01 <47867706+AMike01@users.noreply.github.com>
2023-05-15 11:39:59 +0200

    Update messaggi.php

--- PHP/messaggi.php
+++ PHP/messaggi.php
@@ -5,27 +5,28 @@ use DB\DBAccess;
 
 $connessione = new DBAccess();
 $connessioneRiuscita = $connessione->openDBConnection();
+$target="Elementi_Messaggi";
 $paginaHTML = file_get_contents("../HTML/messaggi.html");
 
-$msg_checked = $_POST['form_msg'];
-if(!empty($msg_checked))
+
+if(isset($_POST['form_msg']))
 {
-    $N = count($msg_checked);
-    for($i=0; $i < $N; $i++)
+    $msg_checked = $_POST['form_msg'];
+    if(!empty($msg_checked))
     {
-        $connessione->Message_Read($msg_checked[$i]);
+        $N = count($msg_checked);
+        for($i=0; $i < $N; $i++)
+        {
+            $connessione->Message_Read($msg_checked[$i]);
+        }
     }
-}
-else    
+}  
+   
+$result=$connessione->getMessages();
+$connessione->closeConnection();
     
 
-    $target="Elementi_Messaggi";
+$paginaHTML = str_replace("Elementi_Messaggi", $result, $paginaHTML);
 
-    $result=$connessione->getMessages();
-    $connessione->closeConnection();
-    
-
-    $paginaHTML = str_replace("Elementi_Messaggi", $result, $paginaHTML);
-
-    echo $paginaHTML;
+echo $paginaHTML;
 ?>
