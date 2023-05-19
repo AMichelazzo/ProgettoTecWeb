3565e0a4bd0e4f44aa65218f1b134e5792967bb3
AMike01 <47867706+AMike01@users.noreply.github.com>
2023-05-19 12:00:11 +0200

    eliminazione tabella e passaggio ad inline
    Aggiunta del campo "email", aggiunta di funziona per eliminazione messaggi

--- PHP/messaggi.php
+++ PHP/messaggi.php
@@ -5,9 +5,10 @@ use DB\DBAccess;
 
 $connessione = new DBAccess();
 $connessioneRiuscita = $connessione->openDBConnection();
+$target="Elementi_Messaggi";
 $paginaHTML = file_get_contents("../HTML/messaggi.html");
 
-if (isset($_POST['form_msg'])) {
+if (isset($_POST['form_msg'])) {   // funzione che segna come letti i messaggi selezionati
     $msg_checked = $_POST['form_msg'];
     if (!empty($msg_checked)) {
         for ($i = 0; $i < count($msg_checked); $i++) {
@@ -16,26 +17,38 @@ if (isset($_POST['form_msg'])) {
     }
 }
 
-$target = "Elementi_Messaggi";
+if(isset($_POST["submit_elimina"])) {  // funzione che elimina i messaggi selezionati 
+    $msg_checked = $_POST['form_msg'];
+    if(!empty($msg_checked))  {
+        for($i=0; $i < count($msg_checked); $i++) {
+            $connessione->delete_Message($msg_checked[$i]);
+        }
+    }
+}
 
-$result = $connessione->getMessages();
-$ElencoMsg = "";
-$connessione->closeConnection();
-    
-for ($i=0; $i<count($result); $i++) {
-    $ElencoMsg.= "<tr><td><input type=\"checkbox\" name=\"form_msg[]\" value=" . $result[$i]["id_messaggio"] .
-        "\"/></td>";
 
-        $ElencoMsg .= "<td>" . $result[$i]["id_messaggio"] ."</td><td>" . $result[$i]["username"] . "</td><td>" .
-        $result[$i]["data"] . "</td><td>" . "<a href=\"../PHP/prodotto.php?prod=".$result[$i]['id_prodotto']."\">".$result[$i]['Nome']."</a>".
-        "</td><td>" . $result[$i]["msg"];
+    $result=$connessione->getMessages();
+    $connessione->closeConnection();
+
+    for($i=0; $i<count($result); $i++) {  // funzione per la creazione dell'inline
+
+        $ElencoMsg .= "<div id=\"messaggi\"><p class=\"inline\"><input type=\"checkbox\" name=\"form_msg[]\" value=" . $result[$i]["id_messaggio"] .
+        "\"/></p>";
+
+        $ElencoMsg .= "<p class=\"inline\"> Email:" . $result[$i]["email"] . "</p>";
+        $ElencoMsg .= "<p class=\"inline\"> Data:" . $result[$i]["data"] . "</p>";
+
+        if(!is_null($result[$i]["id_prodotto"]))  // se è presente un "id prodotto" nel risultato della query lo mostra con il rispettivo link
+            $ElencoMsg .= "<p class=\"inline\"> Prodotto:<a href=\"../PHP/prodotto.php?prod=".$result[$i]['id_prodotto']."\">".$result[$i]['Nome']."</a></p>";
+        
+        $ElencoMsg .= "<p class=\"inline\"> Messaggio:" . $result[$i]["msg"] . "</p>";
 
         if($result[$i]["letto"] == 1)
-            $ElencoMsg .= "<td>Si</td></tr>";
-        else   
-            $ElencoMsg .= "<td>No</td></tr>";
+            $ElencoMsg .= "<p class=\"inline\"> Letto:Si</p></div>";
+        else    
+            $ElencoMsg .= "<p class=\"inline\"> Letto:No</p></div>";
     }
-
+    
     $paginaHTML = str_replace("Elementi_Messaggi", $ElencoMsg, $paginaHTML);
 
     echo $paginaHTML;
