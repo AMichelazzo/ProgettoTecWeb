3b51ab3c13a7df31981fc57411987925f60daae2
Alessio <101570067+alessiobarraco@users.noreply.github.com>
2023-05-27 15:55:13 +0200

    modifiche accessibilità menu hamburger

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,11 @@
 function notHamb() {
     let hambutton = document.getElementById("hambutton");
-    if(hambutton.classList.contains("hambuttonoff")) hambutton.className = "hambuttonon"; 
+    if(hambutton.classList.contains("hambuttonoff")) hambutton.className = "hambuttonon";
     else hambutton.className = "hambuttonoff";
+    if(hambutton.value == "Apri menù amministratore") hambutton.value = "Chiudi menù amministratore";
+    else if(hambutton.value == "Chiudi menù amministratore") hambutton.value = "Apri menù amministratore";
+    else if(hambutton.value == "Apri menù utente") hambutton.value = "Chiudi menù utente";
+    else if(hambutton.value == "Chiudi menù utente") hambutton.value = "Apri menù utente";
 
     let menubar = document.getElementById("menubar");
     if(menubar.classList.contains("menubaroff")) menubar.className = "menubaron"; 
