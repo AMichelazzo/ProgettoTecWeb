160edae0d084d797b77156a8b62702c1349d5c83
Alessio <101570067+alessiobarraco@users.noreply.github.com>
2023-05-29 12:14:45 +0200

    nuovo design

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,15 @@ function notHamb() {
     let hambutton = document.getElementById("hambutton");
     if(hambutton.classList.contains("hambuttonoff")) hambutton.className = "hambuttonon";
     else hambutton.className = "hambuttonoff";
-    if(hambutton.value == "Apri menù amministratore") hambutton.value = "Chiudi menù amministratore";
-    else if(hambutton.value == "Chiudi menù amministratore") hambutton.value = "Apri menù amministratore";
-    else if(hambutton.value == "Apri menù utente") hambutton.value = "Chiudi menù utente";
-    else if(hambutton.value == "Chiudi menù utente") hambutton.value = "Apri menù utente";
+    
+    
+    if(hambutton.alt == "Apri menù amministratore") hambutton.alt = "Chiudi menù amministratore";
+    else if(hambutton.alt == "Chiudi menù amministratore") hambutton.alt = "Apri menù amministratore";
+    else if(hambutton.alt == "Apri menù utente") hambutton.alt = "Chiudi menù utente";
+    else if(hambutton.alt == "Chiudi menù utente") hambutton.alt = "Apri menù utente";
+
+    if(hambutton.src == "img/hamOn.png") hambutton.src = "img/hamOff.png";
+    else hambutton.src = "img/hamOn.png"
 
     let menubar = document.getElementById("menubar");
     if(menubar.classList.contains("menubaroff")) menubar.className = "menubaron"; 
--- prototipo.php
+++ prototipo.php
@@ -1,7 +1,7 @@
 <?php
 $pagina = file_get_contents("HTML/prototipo.html");
-$target = "Nome negozio";
-$titolo = "TITOLO";
-$pagina = str_replace($target, $titolo, $pagina);
+//$target = "Nome negozio";
+//$titolo = "TITOLO";
+//$pagina = str_replace($target, $titolo, $pagina);
 echo $pagina;
 ?>
