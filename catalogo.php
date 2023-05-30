d1d2652d25bbd97082634fb1a1b8ac19c1b20450
AMike01 <47867706+AMike01@users.noreply.github.com>
2023-05-30 15:34:18 +0200

    update catalogo (si veda descrizione)
    Aggiunta funzione per aggiungere nuovi prodotti e nuove categorie (al momento da completare però)
    Modifica ed eliminazione del prodotto funzionanti
    Da implementare messaggio di conferma per eliminazione
    Da implementare eliminazione delle categorie

--- catalogo.php
+++ catalogo.php
@@ -7,53 +7,107 @@ $connessione = new DBAccess();
 $connessioneRiuscita = $connessione->openDBConnection();
 $Elenco_prod = "";
 $target="<!--Elementi_Catalogo-->";
-$result = array();
 $paginaHTML = file_get_contents("HTML/catalogo.html");
 
-// deve funzionare solo se admin!
-if(!isset($_POST["modifica_prod"])) {
-    $result=$connessione->getAllProducts();
-    $connessione->closeConnection();
+if(isset($_POST["submit_modifica_prod"], $_POST["nome_prod"], $_POST["category_id"], $_POST["desc_prod"]))   // chiamata al DB per modifica del prodotto
+    $connessione->modifyProduct($_POST["prod_id"], $_POST["category_id"], $_POST["nome_prod"], $_POST["desc_prod"]);
 
-    $Elenco_prod .= "<form action=\"catalogo.php\" method=\"POST\">";
-    for($i=0; $i<count($result); $i++) {  // funzione per la creazione dell'inline
-
-        $Elenco_prod .= "<div id=\"prodotti\"><p class=\"inline\"><input type=\"checkbox\" name=\"form_id_prodotti[]\" value=" . $result[$i]["id_prodotto"] .
-        "\"/></p>";
-        $Elenco_prod .="<p class=\"inline\"><input type=\"hidden\" name=\"form_id_categoria[]\" value=" . $result[$i]["id_categoria"] . "\"/></p>";
-
-        $Elenco_prod .= "<p class=\"inline\"> Nome prodotto: " . $result[$i]["Prod_Nome"] . " |</p>";
-        $Elenco_prod .= "<p class=\"inline\"> Categoria: " . $result[$i]["Cat_Nome"] . "|</p>";
-        $Elenco_prod .= "<p class=\"inline\"> Descrizione: " . $result[$i]["Descrizione"] . ".</p></div>";
-        $Elenco_prod .= "<p align=\"right\"><input type=\"button\" class=\"invio\" id=\"modifica_prod\" name=\"modifica_prod\" value=\"Modifica\" /></p><br>";
+if(isset($_POST["elimina_prod"])) {  // funzione per l'eliminazione del prodotto
+    $connessione->deleteProduct($_POST["product_id"]);
+    // da implementare messaggio di conferma
     }
 
-    $Elenco_prod .= "<input type=\"button\" id=\"elimina_prod\" name=\"elimina_prod\" class=\"invio\" value=\"Elimina prodotti selezionati\" /></form>";
+if(isset($_POST["submit_new_prod"], $_POST["nome_prod"], $_POST["category_id"], $_POST["desc_prod"])) {
+    $connessione->newProduct($_POST["nome_prod"], $_POST["category_id"], $_POST["desc_prod"]);
 }
-else { // modifica del prodotto
+else {} // da implementare errore in caso di omissione dei campi del prodotto
     
-    $result = $connessione->getProduct($Id_prodotto); // da implementare, come fa a ricevere l'id del prodotto??
-    $paginaHTML = str_replace("Catalogo Prodotti", "Modifica Prodotto", $paginaHTML);
-    
-    $Elenco_prod .= "<form action=\"catalogo.php\" method=\"POST\">";
-    $Elenco_prod .= "<div><label for=\"nome_prod\">Nome prodotto:</label><br>
-    <input type=\"text\" id=\"nome_prod\" name=\"nome_prod\" value=\"" . $result["Nome"] . "\"/></div><br>";
-    $Elenco_prod .= "<div><label for=\"cat_prod\">Categoria prodotto:</label><br>
-    <select name=\"categories\" id=\"categories\"> ";
+
+// da implementare aggiunta immagini?
+
+// deve funzionare solo se admin!
+/* if(user is admin) { !! fare controllo */
+    $result=$connessione->getAllProducts();   // parte del codice che mostra il catalogo
+
+    $Elenco_prod .= "<form action=\"catalogo.php\" method=\"POST\"><p class=\"inline\"><input type=\"submit\" class=\"invio\" id=\"new_product\" name=\"new_product\" value=\"Aggiungi nuovo prodotto\" /></p>";
+    $Elenco_prod .= "<p class=\"inline\"><input type=\"submit\" class=\"invio\" id=\"new_category\" name=\"new_category\" value=\"Aggiungi nuova categoria\" /></p></form><br>";
+
+    for($i=0; $i<count($result); $i++) {  // funzione per la creazione dell'inline
+
+        $Elenco_prod .= "<form action=\"catalogo.php\" method=\"POST\">"; // un form per ogni prodotto
+        $Elenco_prod .= "<p class=\"inline\"><input type=\"hidden\" name=\"product_id\" value=\"" . $result[$i]["id_prodotto"] . "\"/></p>" . // mi salvo l'id_prodotto
+        "<p class=\"inline\"><input type=\"hidden\" name=\"category_id\" value=" . $result[$i]["id_categoria"] . "\"/></p>";    // e l'id_categoria
+
+        $Elenco_prod .= "<p class=\"inline\"> Nome prodotto: " . $result[$i]["Prod_Nome"] . " |</p>";       // informazioni dei prodotti
+        $Elenco_prod .= "<p class=\"inline\"> Categoria: " . $result[$i]["Cat_Nome"] . "|</p>";
+        $Elenco_prod .= "<p class=\"inline\"> Descrizione: " . $result[$i]["Descrizione"] . ".</p>";
+        
+        $Elenco_prod .= "<p class=\"inline\"><input type=\"submit\" class=\"invio\" id=\"modifica_prod\" name=\"modifica_prod\" value=\"Modifica\" /></p>";
+        $Elenco_prod .= "<p class=\"inline\"><input type=\"submit\" class=\"invio\" id=\"elimina_prod\" name=\"elimina_prod\" value=\"Elimina\" /></p><br> </form>";
+    }
+/*}
+else // user non è admin
+{
+    header("Location : index.php");
+} */
+
+if(isset($_POST["modifica_prod"])) {  // funzione che mostra la pagina di modifica del prodotto selezionato
+
+    $result = $connessione->getProduct($_POST["product_id"]); 
+    $paginaHTML = str_replace("Catalogo prodotti", "Modifica Prodotto", $paginaHTML); 
+
+    $Elenco_prod = "<form action=\"catalogo.php\" method=\"POST\">";
+    $Elenco_prod .= "<div><input type=\"hidden\" name=\"prod_id\" value=\"" . $_POST["product_id"] . "\"/></div>";
+    $Elenco_prod .= "<div><label for=\"nome_prod\">Nome prodotto:</label><br></div>
+    <div><input type=\"text\" id=\"nome_prod\" name=\"nome_prod\" value=\"" . $result[0]["Nome"] . "\"/></div>";
+    $Elenco_prod .= "<div><label for=\"cat_prod\">Categoria prodotto:</label><br></div>
+    <select name=\"category_id\" id=\"categories\"> ";
 
     $categories = $connessione->getCategories();
     
-    for($i=0; $i<count($categories); $i++) {  // creazione delle varie categorie
-        $name =  $categories[$i]["Nome"];
-        $Elenco_prod .= "<option value=\"" . $name . "\">" . $name . "</option>";
-    }
-
+    for($i=0; $i<count($categories); $i++)   // creazione delle varie categorie ,  da sistemare, deve mostrare come selezionata la categoria del prodotto
+        $Elenco_prod .= "<option value=\"" . $categories[$i]["id_categoria"] . "\">" . $categories[$i]["Nome"] . "</option>";
+    
     $Elenco_prod .= "</select><div><label for=\"desc_prod\">Descrizione prodotto:</label><br>
-    <input type=\"text\" id=\"desc_prod\" name=\"desc_prod\" value=\"" . $result["Descrizione"] . "\"/></div><br>";
+    <textarea id=\"desc_prod\" name=\"desc_prod\" rows=\"10\" cols=\"40\" maxlength=\"500\">" . $result[0]["Descrizione"]. "</textarea></div><br>";
+    $Elenco_prod .= "<input type=\"submit\" class=\"invio\" id=\"annulla_modifica_prod\" name=\"annulla_modifica_prod\" value=\"Annulla modifiche\"/>";
+    $Elenco_prod .= "<input type=\"submit\" class=\"invio\" id=\"submit_modifica_prod\" name=\"submit_modifica_prod\" value=\"Conferma modifiche\"/></form><br>"; 
+}
 
-    $Elenco_prod= "<input type=\"button\" id=\"submit_modifca_prod\" name=\"submit_modifica_prod\" value=\"Conferma modifiche\"/></form>"; 
-}   
-// Close connection?
+if(isset($_POST["new_product"])) {
+
+    $paginaHTML = str_replace("Catalogo prodotti", "Aggiunta nuovo prodotto", $paginaHTML); 
+
+    $Elenco_prod = "<form action=\"catalogo.php\" method=\"POST\">";
+    $Elenco_prod .= "<div><label for=\"nome_prod\">Nome prodotto:</label><br></div>
+    <div><input type=\"text\" id=\"nome_prod\" name=\"nome_prod\" value=\"\"/></div>";
+    $Elenco_prod .= "<div><label for=\"cat_prod\">Categoria prodotto:</label><br></div>
+    <select name=\"category_id\" id=\"categories\"> ";
+
+    $categories = $connessione->getCategories();
+    
+    for($i=0; $i<count($categories); $i++)   // creazione delle varie categorie ,  da sistemare, deve mostrare come selezionata la categoria del prodotto
+        $Elenco_prod .= "<option value=\"" . $categories[$i]["id_categoria"] . "\">" . $categories[$i]["Nome"] . "</option>";
+    
+    $Elenco_prod .= "</select><div><label for=\"desc_prod\">Descrizione prodotto:</label><br>
+    <textarea id=\"desc_prod\" name=\"desc_prod\" rows=\"10\" cols=\"40\" maxlength=\"500\"></textarea></div><br>";
+    $Elenco_prod .= "<input type=\"submit\" class=\"invio\" id=\"annulla_new_prod\" name=\"annulla_new_prod\" value=\"Annulla creazione prodotto\"/>";
+    $Elenco_prod .= "<input type=\"submit\" class=\"invio\" id=\"submit_new_prod\" name=\"submit_new_prod\" value=\"Conferma nuovo prodotto\"/></form><br>"; 
+}
+
+if(isset($_POST["new_category"])) {
+
+    $paginaHTML = str_replace("Catalogo prodotti", "Aggiunta nuova categoria", $paginaHTML); 
+
+    $Elenco_prod = "<form action=\"catalogo.php\" method=\"POST\">";
+    $Elenco_prod .= "<div><label for=\"nome_category\">Nome categoria:</label><br></div>
+    <div><input type=\"text\" id=\"nome_category\" name=\"nome_category\" value=\"\"/></div>" . "<div><label for=\"desc_prod\">Descrizione prodotto:</label><br>";
+    $Elenco_prod .= "<textarea id=\"desc_prod\" name=\"desc_prod\" rows=\"10\" cols=\"40\" maxlength=\"500\"></textarea></div><br>";
+    $Elenco_prod .= "<input type=\"submit\" class=\"invio\" id=\"annulla_new_cat\" name=\"annulla_new_cat\" value=\"Annulla creazione categoria\"/>";
+    $Elenco_prod .= "<input type=\"submit\" class=\"invio\" id=\"submit_new_cat\" name=\"submit_new_cat\" value=\"Conferma nuova categoria\"/></form><br>"; 
+}
+
+
 $paginaHTML = str_replace($target, $Elenco_prod, $paginaHTML);
 echo $paginaHTML;
 ?>
