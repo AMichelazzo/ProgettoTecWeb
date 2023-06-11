ea1b0b56be839badd855a8bb1fcc778656d1137e
Alessio <101570067+alessiobarraco@users.noreply.github.com>
2023-06-11 17:28:56 +0200

    nuove funzioni

--- PHP/class.php
+++ PHP/class.php
@@ -7,8 +7,7 @@ class Access
 {
     public static function getUtenti() /*solo quelli con ruolo user*/
     {
-        $result = DBAccess::dbQuery("SELECT * FROM utente WHERE ruolo = 'user'");
-        return $result;
+        return DBAccess::dbQuery("SELECT * FROM utente WHERE ruolo = 'user'");
     }
 
     public static function deleteUtente($username)
@@ -16,49 +15,47 @@ class Access
         return DBAccess::dbQuery("DELETE FROM utente WHERE username = ? AND ruolo = 'user'", $username);
     }
 
-    public static function getCategories() 
+    public static function getCategories()
     {
         return DBAccess::dbQuery("SELECT * FROM 'categoria'");
     }
 
-    public static function getCategory($id_categoria) 
+    public static function getCategory($id_categoria)
     {
         return DBAccess::dbQuery("SELECT * FROM categoria WHERE id_categoria = ?", $id_categoria);
     }
 
-    public static function getCategoryName($id_categoria) {
+    public static function getCategoryName($id_categoria)
+    {
+        $result = DBAccess::dbQuery("SELECT DISTINCT Nome FROM categoria WHERE id_categoria = ?", $id_categoria);
+        if ($result !== false && $result !== null) {
+            $result = $result[Nome];
+        }
+        return $result;
+    }
 
+    public static function newCategory($nome, $descrizione)
+    {
+        return DBAccess::dbQuery("INSERT INTO categoria(Nome, Descrizione) VALUES (?, ?)", $nome, $descrizione);
+    }
+
+    public static function modifyCategory($id_categoria, $nome, $descrizione)
+    {
+        return DBAccess::dbQuery("UPDATE categoria SET Nome = ?, Descrizione = ? WHERE id_categoria = ?", $nome, $descrizione, $id_categoria);
+    }
+
+    public static function deleteCategory($id_categoria)
+    {
+        return DBAccess::dbQuery("DELETE FROM categoria WHERE id_categoria = ?", $id_categoria);
+    }
+
+    public static function getProductsbyCategory($id_categoria)
+    {
+        return DBAccess::dbQuery("SELECT * FROM prodotti WHERE id_categoria = ?", $id_categoria);
+    }
+
+    public static function getProduct($id_prodotto)
+    {
+        return DBAccess::dbQuery("SELECT prodotti.id_prodotto, prodotti.id_categoria, prodotti.Nome, prodotti.Descrizione, immagini.path, immagini.alt_img FROM prodotti LEFT JOIN immagini on prodotti.id_prodotto = immagini.id_prodotto WHERE prodotti.id_prodotto = ?", $id_prodotto);
     }
 }
-/*
-class Utente
-{
-    public static function get()
-    {
-        return DBAccess::dbQuery("SELECT * FROM utente WHERE ruolo = 'user'");
-    }
-    
-    public static function isAdmin($username)
-    {
-        //if (!isset) return false;
-        $amministratore = DBAccess::dbQuery("SELECT * FROM utente WHERE username = ? ruolo = 'admin'", $username);
-        $amministratore = $amministratore['username'];
-        return $amministratore == $username; /*ERRORE ASSEGNAZIONE AMMINISTRATORE*/
-    /*}
- 
-    public static function isUser($username)
-    {
-        return DBAccess::dbQuery("SELECT * FROM utente WHERE username = ? ruolo = 'user'", $username) === $username;
-    }
-
-    public static function insert($username, $password, $email)
-    {
-        return DBAccess::dbQuery("INSERT INTO utente (username, password, email, ruolo, data_creazione) VALUES (?, ?, ?, current_timestamp())", $username, $password, $email);
-    }
-
-    public static function delete($username)
-    {
-        return DBAccess::dbQuery("DELETE FROM utente WHERE username = ? AND ruolo = 'user'", $username);
-    }
-}*/
-?>
