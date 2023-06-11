587f226899beae0ef895624577d50434d0a3de51
Alessio <101570067+alessiobarraco@users.noreply.github.com>
2023-06-11 17:28:25 +0200

    gestione errore utenti

--- utenti.php
+++ utenti.php
@@ -11,35 +11,36 @@ $utenti = "";
 $stringaUtenti = "";
 
 $utenti = Access::getUtenti();
-if (!empty($utenti)) {
-    foreach ($utenti as $utente) {
-        $stringaUtenti .= '<div class="utenti">
-            <div class="flexutente">
-            <div><span lang="en">Username</span>: ' . $utente['username'] . ' </div>
-            <div><span lang="en">Email</span>: ' . $utente['email'] . ' </div>
-            <form action="utenti.php" method="post">
-            <input type="hidden" id="userId" name="userId" value="' . $utente['username'] . '"/>
-            <input type="submit" id="delete" name="delete" class="invio" value="Elimina"/>
-            </form>
-        </div>
-    </div>';
+if ($utenti !== false) {
+    if (!empty($utenti)) {
+        foreach ($utenti as $utente) {
+            $stringaUtenti .= '<div class="utenti">
+                <div class="flexutente">
+                <div><span lang="en">Username</span>: ' . $utente['username'] . ' </div>
+                <div><span lang="en">Email</span>: ' . $utente['email'] . ' </div>
+                <form action="utenti.php" method="post">
+                <input type="hidden" id="userId" name="userId" value="' . $utente['username'] . '"/>
+                <input type="submit" id="delete" name="delete" class="invio" value="Elimina"/>
+                </form>
+            </div>
+        </div>';
         }
+    } else {
+        $stringaUtenti =
+        '<div class="utenti">
+            <div class="flexutente">
+                Nessun utente è presente nel sistema.
+            </div>
+        </div>';
+    }
 } else {
-    $stringaUtenti =
-    '<div class="utenti">
-        <div class="flexutente">
-            Nessun utente è presente nel sistema.
-        </div>
-    </div>';
-}
-/*} else {
     $stringaUtenti =
         '<div class="utenti">
             <div class="flexutente">
                 Il sistema non è al momento raggiungibile, ci scusiamo per il disagio.
             </div>
         </div>';
-}*/
+}
 
 echo str_replace("<utenti/>", $stringaUtenti, $paginaHTML);
 ?>
