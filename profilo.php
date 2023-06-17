e658cc8e731f5bb255e4c60b6a37112b8eb82d01
Alcky26 <Alchy.26.07@gmail.com>
2023-06-17 15:09:30 +0200

    passchange
    per ora cambia password ma i messaggi di errore devo ancora sistemarli

--- profilo.php
+++ profilo.php
@@ -16,34 +16,105 @@ else
 $paginaHTML .= file_get_contents("HTML/profilo.html");
 $result = "";
 
-if(isset($_SESSION["username"]) && isset($_POST["old_password"])  && isset($_POST["new_password"]) 
-    && isset($_POST["new_password_repeat"]) && isset($_POST["cambia_password"])) {
-
-        if($_POST["new_password"] == $_POST["new_password_repeat"]) {
-
-            $result = Access::checkOldPassword($_SESSION["username"], $_POST["old_password"]);
-            
-            if($result) {
-            $result2 =  Access::ChangePassword($_SESSION["username"], $_POST["old_password"], $_POST["new_password"]);
-            header("Location: ../profilo.php");
+if(isset($_SESSION["username"]) && isset($_POST["old_password"])  && isset($_POST["new_password"]) && isset($_POST["new_password_repeat"]) && isset($_POST["cambia_password"]) && !$_SESSION["changeExec"]) {
+    $result = Access::checkOldPassword($_SESSION["username"], $_POST["old_password"]);
+    if(isset($result) && $result){
+        if( $_POST["new_password"]!=$_POST["old_password"]){
+            if($_POST["new_password"] == $_POST["new_password_repeat"]) {    
+                $result2 =  Access::ChangePassword($_SESSION["username"], $_POST["old_password"], $_POST["new_password"]);
+                if(isset($result2)&&$result2){
+                    $_SESSION["changeExec"]=true;
+                }
+                else{
+                    $_SESSION["error_generic"]='<div id="msgchange" class="change-error" role="alert">Errore nella modifica della password.</div>';
+                    $_SESSION["error_generic2"]="<img id=\"errore_generico\" src=\"img/Xrossa.png\" alt=\"Errore nella modifica della password.\" height=\"15px\" width=\"15px\"/>";
+                }
+                header("Location: profilo.php");
+            }
+            else {
+                $_SESSION["error_new_pass"]='<div id="msgchange" class="change-error" role="alert">Le due nuove <span lang=\en\">password</span> non sono uguali.</div>';
+                $_SESSION["error_new_pass2"]="<img id=\"passNOT_combaciano\" src=\"img/Xrossa.png\" alt=\"Le due nuove <span lang=\en\">password</span> non sono uguali.
+                \ height=\"15px\" width=\"15px\"/>";
+                header("Location: profilo.php");
             }
         }
         else {
-            $_SESSION["error_new_pass"]="<img id=\"passNOT_combaciano\" src=\"img/Xrossa.png\" alt=\"Le due nuove <span lang=\en\">password</span> non sono uguali.
+            $_SESSION["error_new_old"]='<div id="msgchange" class="change-error" role="alert">La nuova <span lang=\en\">password</span> non può combaciare con quella nuova.</div>';
+            $_SESSION["error_new_old2"]="<img id=\"passuguale\" src=\"img/Xrossa.png\" alt=\"La nuova <span lang=\en\">password</span> non può combaciare con quella nuova.
             \ height=\"15px\" width=\"15px\"/>"; 
-                header("Location: ../profilo.php");
-        }
-        if(isset($result)) {
-            $_SESSION["error_old_pass"]=$result;
-            $_SESSION["error_old_pass2"]="<img id=\"old_passwordNOT_giusta\" src=\"img/Xrossa.png\" alt=\"Vecchia password sbagliata.\" height=\"15px\" width=\"15px\"/>";
-        }
-        else if(isset($result2)) {
-            $_SESSION["error_generic"]=$result2;
-            $_SESSION["error_generic2"]="<img id=\"errore_generico\" src=\"img/Xrossa.png\" alt=\"Errore nella modifica della password.\" height=\"15px\" width=\"15px\"/>";
-        
+            header("Location: profilo.php");
         }
     }
+    else{
+        $_SESSION["error_old_pass"]='<div id="msgchange" class="change-error" role="alert">Vecchia <span lang=\en\">password</span> errata.</div>';
+        $_SESSION["error_old_pass2"]="<img id=\"old_passwordNOT_giusta\" src=\"img/Xrossa.png\" alt=\"Vecchia password errata.\" height=\"15px\" width=\"15px\"/>";
+        header("Location: profilo.php");
+    }
+}
+if(isset($_SESSION["changeExec"])&&$_SESSION["changeExec"])
+{
+    $paginaHTML = str_replace('<div id="msgchange" role="alert"></div>', '<div id="msgchange" class="change-success" role="alert">Modifica avvenuta con successo.</div>', $paginaHTML);
+}
 
+if(isset($_SESSION['error_generic'])&&isset($_SESSION['error_generic2']))
+{
+    $errgen=$_SESSION['error_generic2'];
+}
+else
+{
+    $errgen=null;
+}
+
+if(isset($_SESSION['error_new_pass'])&&isset($_SESSION['error_new_pass2']))
+{
+    $errNonC=$_SESSION['error_new_pass2'];
+}
+else
+{
+    $errNonC=null;
+}
+if(isset($_SESSION['error_new_old'])&&isset($_SESSION['error_new_old2']))
+{
+    $errUguale=$_SESSION['error_new_old2'];
+}
+else
+{
+    $errUguale=null;
+}
+if(isset($_SESSION['error_old_pass'])&&isset($_SESSION['error_old_pass2']))
+{
+    $errOPW=$_SESSION['error_old_pass2'];
+}
+else
+{
+    $errOPW=null;
+}
+
+
+if (isset($errgen)){
+    $paginaHTML = str_replace("<!--generico-->", $errgen, $paginaHTML);
+    $paginaHTML = str_replace('<div id="msgchange" role="alert"></div>', $_SESSION['error_generic'], $paginaHTML);
+    unset($_SESSION["error_generic"]);
+    unset($_SESSION["error_generic2"]);
+}
+if (isset($errNonC)){
+    $paginaHTML = str_replace("<!--nonCorrisp-->", $errNonC, $paginaHTML);
+    $paginaHTML = str_replace('<div id="msgchange" role="alert"></div>', $_SESSION['error_new_pass'], $paginaHTML);
+    unset($_SESSION["error_new_pass"]);
+    unset($_SESSION["error_new_pass2"]);
+}
+if (isset($errUguale)){
+    $paginaHTML = str_replace("<!--oldnew-->", $errUguale, $paginaHTML);
+    $paginaHTML = str_replace('<div id="msgchange" role="alert"></div>', $_SESSION['error_new_old'], $paginaHTML);
+    unset($_SESSION["error_new_old"]);
+    unset($_SESSION["error_new_old2"]);
+}
+if (isset($errOPW)){
+    $paginaHTML = str_replace("<!--old-->", $errOPW, $paginaHTML);
+    $paginaHTML = str_replace('<div id="msgchange" role="alert"></div>', $_SESSION['error_old_pass'], $paginaHTML);
+    unset($_SESSION["error_old_pass"]);
+    unset($_SESSION["error_old_pass2"]);
+}
 echo $paginaHTML;
 
 ?>
