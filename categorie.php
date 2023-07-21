22de00ed6d20579e932cede84af874540047ffc3
Alessio <101570067+alessiobarraco@users.noreply.github.com>
2023-07-21 18:22:10 +0200

    modifica categorie

--- categorie.php
+++ categorie.php
@@ -5,7 +5,7 @@ if (isset($_SESSION["username"])) {
     $template=Access::getHeader("Categorie", "Lista delle categorie presenti nel nostro catalogo di oggetti.", "Categorie, Prodotti, Oggettistica di vetro, Lista", $_SESSION["username"], $_SESSION["ruolo"], "Categorie");
 }
 else{
-    $template=Access::getHeader("Categorie", "Lista delle categorie presenti nel nostro catalogo di oggetti.", "Categorie, Prodotti, Oggettistica di vetro, Lista", "guest", "guest", "Categorie");
+    $template=Access::getHeader("Categorie", "Lista delle categorie presenti nel nostro catalogo di oggetti.", "Categorie, Prodotti, Oggettistica di vetro, Lista");
 }
 
 $template .= file_get_contents('HTML/categorie.html');
@@ -14,45 +14,39 @@ $replace = "";
 //SE HO CLICCATO PRODOTTI DA QUALSIASI PAGINA
 if (!isset($_GET["cat"])) {
     $result = $nome = $descrizione = "";
-    $result=Access::getCategories();
+    $result = Access::getCategories();
     $ElencoCateg="";
     for ($i = 0; $i < count($result); $i++) {
-        $ElencoCateg=$ElencoCateg."<div><a href=\"categorie.php?cat=".$result[$i]['id_categoria']."\">".$result[$i]['Nome']."</a></div>
-                        <div>".$result[$i]['Descrizione']."</div> ";
+        $ElencoCateg .= '<div><a href="categorie.php?cat=' . $result[$i]['id_categoria'] . '">' . $result[$i]['Nome'] . '</a></div>
+        <div>' . $result[$i]['Descrizione'] . '</div> ';
     }
-    $replace = array("<!--categories_place_holder -->" =>$ElencoCateg 
-                    );     
+    $replace = array("<!--categories_place_holder -->" =>$ElencoCateg);
 }
 //SE HO SELEZIONATO UNA CATEGORIA
-if (isset($_GET["cat"])) { 
-
+if (isset($_GET["cat"])){
     $result = $nome = $descrizione = "";
     $result=Access::getProductsbyCategory($_GET["cat"]);
-    if(count($result)>0){
+    if(!empty($result)){
         $ElencoProdot="";
         for ($i = 0; $i < count($result); $i++) {
-            $ElencoProdot=$ElencoProdot."<div><a href=\"prodotto.php?prod=".$result[$i]['id_prodotto']."\">".$result[$i]['Nome']."</a></div>
-                            <div>".$result[$i]['Descrizione']."</div>
-                            <div><img <lt=\"\"></img></div> ";
+            $ElencoProdot .= '<div><a href="prodotto.php?prod=' . $result[$i]['id_prodotto'] . '">' . $result[$i]['Nome'] . '</a></div>
+            <div>' . $result[$i]['Descrizione'] . '</div>' . '<div><img alt=""></img></div> ';
         }
-        $replace = array("<!--categories_place_holder -->" =>$ElencoProdot 
-                    ); 
+        $replace = array("<!--categories_place_holder -->" =>$ElencoProdot);
     }
     else{
         $result = $nome = $descrizione = "";
         $result=Access::getCategories();
         $ElencoCateg="";
         for ($i = 0; $i < count($result); $i++) {
-            $ElencoCateg=$ElencoCateg."<div><a href=\"categorie.php?cat=".$result[$i]['id_categoria']."\">".$result[$i]['Nome']."</a></div>
-                            <div>".$result[$i]['Descrizione']."</div> ";
+            $ElencoCateg .= '<div><a href="categorie.php?cat=' . $result[$i]['id_categoria'] . '">' . $result[$i]['Nome'] . '</a></div>' . '<div>' . $result[$i]['Descrizione'] . '</div> ';
         }
-        $replace = array("<!--categories_place_holder -->" =>$ElencoCateg 
-                        ); 
+        $replace = array("<!--categories_place_holder -->" =>$ElencoCateg);
     }
 }
 
-
-    foreach($replace as $key => $value) 
-            $HTMLPage = str_replace($key, $value, $template);
+foreach($replace as $key => $value) {
+    $HTMLPage = str_replace($key, $value, $template);
+}
 echo $HTMLPage;
 ?>
