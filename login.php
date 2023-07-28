fa8b05598c42db567ea07e92b9adb2264737c9fe
Alessio <101570067+alessiobarraco@users.noreply.github.com>
2023-07-28 10:51:15 +0200

    Breadcrumb modificata

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ if (isset($_SESSION["username"])) {
     header("Location: index.php");
 } else {
     require_once "PHP/class.php";
-    $template = Access::getHeader("Login", "Accedi al tuo account.", "accedi, login, registrati, signup, crea account, username, email", "guest", "guest", "Login</span>");
+    $template = Access::getHeader("Login", "Accedi al tuo account.", "accedi, login, registrati, signup, crea account, username, email");
 }
 $template = str_replace('<div id="riservata"><a href="login.php">Area Riservata</a></div>', '', $template);
 $template .= file_get_contents('HTML/login.html');
